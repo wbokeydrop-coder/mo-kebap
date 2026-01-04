5ea61f8efa47425ecc0e380d6a678ac9d050025a
MO KEBAP Owner <owner@mo-kebap.de>
2026-01-04 17:05:58 +0100

    Fix: force global Node.js runtime for Cloudflare Pages static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { runtime: 'nodejs' },
   output: 'export',
-  images: {
-    unoptimized: true
-  }
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
