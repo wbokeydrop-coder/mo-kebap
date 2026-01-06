215a6781cc693e9e9a2e3433ebc4d3893d396a48
MO KEBAP Owner <owner@mo-kebap.de>
2026-01-06 14:06:52 +0100

    Resize below-nav AdSense banner

--- src/components/ads/HomeTopAd.tsx
+++ src/components/ads/HomeTopAd.tsx
@@ -2,8 +2,8 @@ import { AdSlot } from './AdSlot'
 
 export function HomeTopAd() {
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '16px 0', padding: '0 8px' }}>
-      <div style={{ width: '100%', maxWidth: 728 }}>
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '16px 0' }}>
+      <div style={{ width: 728, maxWidth: '100%', minHeight: 90 }}>
         <AdSlot adUnitId="5215359075" minHeight={90} />
       </div>
     </div>
